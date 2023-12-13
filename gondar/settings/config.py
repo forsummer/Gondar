de1a58eaf37a45ebbdec0ae67429f0dc94bc21b6
Boman Ng <boman.ng@qq.com>
2023-12-13 15:26:49 +0000

    add: network config

--- gondar/settings/config.py
+++ gondar/settings/config.py
@@ -17,3 +17,8 @@ class NetworkConfig(Enum):
     HTTP_PROXY: str | None = None
     HTTPS_PROXY: str | None = None
     FTP_PROXY: str | None = None
+
+    MAX_RETRY: int | None = 3
+    RETRY_GAP: int | None = 5
+
+    TIMEOUT: int | None = 120
