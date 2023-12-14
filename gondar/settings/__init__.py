d0e47226842a9fb87b209840ec35bf3ff5b4b511
Boman Ng <boman.ng@qq.com>
2023-12-14 06:03:34 +0000

    add: load custom conf from .env

--- gondar/settings/__init__.py
+++ gondar/settings/__init__.py
@@ -1,6 +1,10 @@
 import os
 
+from dotenv import load_dotenv
+
 from gondar.settings.config import IdentityConfig, NetworkConfig
 
 os.environ.update(**IdentityConfig)
 os.environ.update(**NetworkConfig)
+
+load_dotenv(verbose=True)
